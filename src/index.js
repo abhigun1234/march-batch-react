7b8bd50e617c904894a11a964cd9d9830e66b6fe
Abhishek <abhi.ghosh27feb@gmail.com>
2021-03-28 12:51:07 +0530

    add

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "jquery"
+import "popper.js/dist/umd/popper"
+import "bootstrap/dist/js/bootstrap"
+import "bootstrap/dist/css/bootstrap.css"
+
 
 ReactDOM.render(
   <React.StrictMode>
